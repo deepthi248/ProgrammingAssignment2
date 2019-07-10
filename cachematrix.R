b8235f32cdfb1ad6ae05b601091ec228af45c9cc
deepthi <deepthibhogaraju.3@gmail.com>
2019-07-10 15:29:29 +0530

    cacheSolve function is written with the desired comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,17 +1,26 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function     
+##  makeCacheMatrix takes a matrix as input and can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) { # function which takes matrix as input 
-  inverseCheck<- NULL
+  inverseValue<- NULL
   
-  setMatrix <- function(y){ # this function sets the matrix 
-    x <<- y                 # assigning matrix to x which is in different environment
-    inverseCheck <<- NULL        #if the matrix is different from that of existing, assign inverse to NULL
+  setMatrix<- function(y){ ## this function sets the matrix 
+    x <<- y                 ## assigning matrix to x which is in different environment
+    inverseValue <<- NULL        #if the matrix is different from that of existing, assign inverse to NULL
   }
   
+  getMatrix <- function()  x ##get function returns the value of the matrix
+    
+  setMatrixInverse <- function(inverse) inv <<- inverseValue  ## by passing inverse value as argument &setting inverseValue in parent environment
+  GetMatrixInverse <- function() inverseValue                     ## gets the value of inv where called
   
+  
+  list(setMatrix = setMatrix, 
+       getMatrix = getMatrix, 
+       setMatrixInverse = setMatrixInverse,
+       GetMatrixInverse = GetMatrixInverse)                 ## the values are set as list in order to access with input matrix with $ symbol from parent environment
   
 }
 
@@ -20,5 +29,19 @@ makeCacheMatrix <- function(x = matrix()) { # function which takes matrix as inp
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  
+  inverseValue <- x$GetMatrixInverse()              #here we will set inversevalue to the value from getMatrixInverse function
+  if(!is.null(inverseValue)){                       #checking the inverse value if its nullor not
+    message("getting cached data")                  #display message
+    return(inverseValue)                            #returning the value
+  }
+   getmatrixvalue <- x$getMatrix()                  #getting the matrix
+   inversevalue<- solve(getmatrixvalue,...)         #assigning solve function with matrix as input argument to inversevalue 
+   x$setMatrixInverse(inversevalue)                 #setting the inverse value 
+   inversevalue
 }
-f33371de67266e3f8be231b12c60b066b00e55f4
+B<- matrix(c(1,2,3,4),2,2)
+
+B1 <- makeCacheMatrix(B)
+cacheSolve(B1)
+
