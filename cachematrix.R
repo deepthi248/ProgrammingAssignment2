d7c0eb51226db0d987941f6e285d9b53104871f7
deepthi <deepthibhogaraju.3@gmail.com>
2019-07-10 14:38:28 +0530

    matrix set function is written with "<<- " operator

--- cachematrix.R
+++ cachematrix.R
@@ -3,8 +3,16 @@
 
 ## Write a short comment describing this function     
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()) { # function which takes matrix as input 
+  inverseCheck<- NULL
+  
+  setMatrix <- function(y){ # this function sets the matrix 
+    x <<- y                 # assigning matrix to x which is in different environment
+    inverseCheck <<- NULL        #if the matrix is different from that of existing, assign inverse to NULL
+  }
+  
+  
+  
 }
 
 
@@ -13,3 +21,4 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
 }
+f33371de67266e3f8be231b12c60b066b00e55f4
