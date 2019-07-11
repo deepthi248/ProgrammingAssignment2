5fa73cbf5d4ec5b4c4c88e11eb2a64ed54950a17
deepthi <deepthibhogaraju.3@gmail.com>
2019-07-11 09:18:11 +0530

    final

--- cachematrix.R
+++ cachematrix.R
@@ -40,8 +40,3 @@ cacheSolve <- function(x, ...) {
    x$setMatrixInverse(inversevalue)                 #setting the inverse value 
    inversevalue
 }
-B<- matrix(c(1,2,3,4),2,2)
-
-B1 <- makeCacheMatrix(B)
-cacheSolve(B1)
-
